6085e1edb806034c0ef3f250e34e489681725530
Rijk <r.e.hogenbirk@gmail.com>
2018-10-04 10:28:36 +0900

    Made new file for peak detection script

---
+++ peak_detection.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Detection of Lorentzian peaks
+
+Created on Thu Oct  4 10:25:59 2018
+
+@author: Rijk
+
+
+Goal: Get center frequency of Lorentzian peaks within a frequency spectrum
+
+"""
+
