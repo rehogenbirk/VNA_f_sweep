10d58c6b0311e2ec1fb5dc56430325b03584bf24
Rijk <r.e.hogenbirk@gmail.com>
2018-10-10 13:27:33 +0900

    Made stub for calibration script
    Used to set Average to 0 dB when only measuring the through line to accurately measure the dips

---
+++ VNA_calibration.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  9 14:26:23 2018
+
+@author: Rijk
+"""
+
+# Calibrates the VNA when the chip is at 10 K, i.e. superconducting throughline without MKID dips in spectrum
+
