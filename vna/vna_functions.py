9b382671ccdff3ad8d6bb4463b30ab9e4e5bdb99
Rijk <r.e.hogenbirk@gmail.com>
2018-10-11 17:56:39 +0900

    Separated start and stop frequency functions and added IF bandwidth func

--- vna/vna_functions.py
+++ vna/vna_functions.py
@@ -61,7 +61,7 @@ def meas_create(instrument, meas_name, s_parameters, channel_num=1):
     command = ':CALCulate%d:PARameter:DEFine:EXTended "%s","%s"' % (channel_num, meas_name, s_parameters)
     instrument.write(command)
     
-def meas_feed(instrument, meas_name, window_num=1, trace_num=9):
+def meas_show(instrument, meas_name, window_num=1, trace_num=9):
     """Feeds the measuremnt to a window as a numbered trace, so it's displayed there"""
     command = ':DISPlay:WINDow%d:TRACe%d:FEED "%s"' % (window_num, trace_num, meas_name)
     instrument.write(command)
@@ -80,18 +80,33 @@ def set_amplitude(instrument, amplitude, unit='DBM', channel_num=1):
     command = ':SOURce%d:POWer:LEVel:IMMediate:AMPLitude %G %s' % (channel_num, amplitude, unit)
     instrument.write(command)
     
-def set_Df_sweep(instrument, f_start, f_stop, unit='MHZ', channel_num=1):
-    """Sets start and stop frequency of sweep of channel"""
-    command1 = ':SENSe%d:FREQuency:STARt %G %s' % (channel_num, f_start, unit)
-    command2 = ':SENSe%d:FREQuency:STOP %G %s' % (channel_num, f_stop, unit)
-    instrument.write(command1)
-    instrument.write(command2)
+#def set_Df_sweep(instrument, f_start, f_stop, unit='MHZ', channel_num=1):
+#    """Sets start and stop frequency of sweep of channel"""
+#    command1 = ':SENSe%d:FREQuency:STARt %G %s' % (channel_num, f_start, unit)
+#    command2 = ':SENSe%d:FREQuency:STOP %G %s' % (channel_num, f_stop, unit)
+#    instrument.write(command1)
+#    instrument.write(command2)
+    
+def set_f_start(instrument, f_start, unit='MHZ', channel_num=1):
+    """Sets start frequency of sweep of channel"""
+    command = ':SENSe%d:FREQuency:STARt %G %s' % (channel_num, f_start, unit)
+    instrument.write(command)
+    
+def set_f_stop(instrument, f_stop, unit='MHZ', channel_num=1):
+    """Sets stop frequency of sweep of channel"""
+    command = ':SENSe%d:FREQuency:STOP %G %s' % (channel_num, f_stop, unit)
+    instrument.write(command)
 
 def set_num_points(instrument, num_points='MAXimum', channel_num=1):
     """Sets number of data points for a given channel"""
     command = ':SENSe%d:SWEep:POINts %s' % (channel_num, num_points)
     instrument.write(command)
-
+    
+def set_if_bandwidth(instrument, if_bandwidth, window_num=1, channel_num=1):
+    """"Sets the bandwidth in Hz of the intermediate frequency (IF) bandpass filter, low values lead to slow measurements"""
+    command = ':SENSe%s:BANDwidth:RESolution %G HZ' % (window_num, if_bandwidth)
+    instrument.write(command)
+    
 def save_local(instrument, file_path, file_type='CSV Formatted Data', scope='Auto', file_format='Displayed', selector=1):
     command = ':MMEMory:STORe:DATA "%s","%s","%s","%s",%d' % (file_path, file_type, scope, file_format, selector)
     instrument.write(command)
@@ -110,6 +125,11 @@ def get_num_points(instrument):
     command = ':SENSe:SWEep:POINts?'
     num_points = instrument.query_ascii_values(command)[0]
     return num_points
+
+def get_if_bandwidth(instrument):
+    command = ':SENSe:BANDwidth:RESolution?'
+    if_bandwidth = instrument.query_ascii_values(command)[0]
+    return if_bandwidth
 
 
 
