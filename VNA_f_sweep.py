253f5590365176e8eb6968c70fc7485413a98717
Rijk <r.e.hogenbirk@gmail.com>
2018-10-11 17:58:26 +0900

    Added comment with allowed values for parameters

--- VNA_f_sweep.py
+++ VNA_f_sweep.py
@@ -1,8 +1,18 @@
 """
+Rijk Hogenbirk, started on 24/09/18
+
 Description:
 Script to let the VNA perform a measurement of S-parameters with a variable frequency and a set amplitude.
 
-Rijk Hogenbirk, started on 24/09/18
+Allowed input values for Agilent PNA E8361C:
+
+f_start, f_stop:    Between 10 MHz and 67 MHz
+IF_bandwith:        1, 2, 3, 5, 7, 10, 15, 20, 30, 50, 70, 100, 150, 200, 300, 500, 
+                    700, 1k, 1.5k, 2k, 3k, 5k, 7k, 10k, 15k, 20k, 30k, 35k, 40k
+num_points:         Between 1 and 32001
+
+
+
 """
 
 # =============================================================================
