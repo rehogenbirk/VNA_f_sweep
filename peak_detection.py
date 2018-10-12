4bf53e2d5c15d90ae9db4884447e4fb843102b02
Rijk <r.e.hogenbirk@gmail.com>
2018-10-12 18:13:51 +0900

    Implemented np.gradient

--- peak_detection.py
+++ peak_detection.py
@@ -14,39 +14,34 @@ Goal: Get center frequency of Lorentzian peaks within a frequency spectrum
 import numpy               as np
 import matplotlib.pyplot   as plt
 
-# Generate data
+## Generate data
 
 xs = np.linspace(-1, 50)
 
 ys = xs**3
+der_exact = 3*xs**2
+der2_exact = 6*xs
 
-def derivative(xs, ys):
-    """Takes numerical derivative of y(x)"""
-    dxs = np.diff(xs)
-    dys = np.diff(ys)
-    der = dys/dxs
-    return der
+dx = xs[1] - xs[0]
 
-def derivative2(xs, ys):
-    """Takes second numerical derivative of y(x)"""
-    der1 = derivative(xs, ys)
-    der2 = derivative(xs[1:], der1)
-    return der2
 
-der_y = np.zeros([len(xs), 2])
-
-der_y[:, 0]   = derivative(xs, ys)
-der_y[:, 1]  = derivative2(xs, ys)
+der = np.gradient(ys, dx)
+der2 = np.gradient(der, dx)
 
 #der_y = np.insert(der_y, 0, 0) # Inserts a zero at the start
 
 plt.figure(2)
 
-plt.plot(xs[1:], der_y[0])
-plt.plot(xs[2:], der_y[1])
+plt.plot(xs, der_exact)
+plt.plot(xs, der)
+
+plt.figure(3)
+
+plt.plot(xs, der2_exact)
+plt.plot(xs, der2)
 
 # Format the figures
-plt.axis([np.min(xs[2:]), np.max(xs[2:]), np.min(der_y), np.max(der_y)]) # Sets origin in upper-left corner
+#plt.axis([np.min(xs[2:]), np.max(xs[2:]), np.min(der_y), np.max(der_y)]) # Sets origin in upper-left corner
 plt.grid()
 #plt.xlabel('Frequency (%s)' % (f_unit))
 #plt.ylabel('Magnitude (%s)' % ('dB'))
