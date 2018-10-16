b6df099bc74de90dbc11997f65c9d511bfeb21d3
Rijk <r.e.hogenbirk@gmail.com>
2018-10-16 14:39:11 +0900

    Added todo for next step

--- VNA_calibration.py
+++ VNA_calibration.py
@@ -7,3 +7,5 @@ Created on Tue Oct  9 14:26:23 2018
 
 # Calibrates the VNA when the chip is at 10 K, i.e. superconducting throughline without MKID dips in spectrum
 
+# Test Command Expert Sequence and then export it here and turn it into usable functions
+
