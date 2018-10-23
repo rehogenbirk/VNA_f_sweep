c14d85340482da3e97c79628b43eda3f5160ce0d
Rijk <r.e.hogenbirk@gmail.com>
2018-10-23 17:23:36 +0900

    Tested some basic VNA function. leading to some changes in the code

--- test_VNA.py
+++ test_VNA.py
@@ -5,31 +5,31 @@ Created on Tue Oct  2 16:04:35 2018
 @author: Rijk
 """
 
+# =============================================================================
+# ## Import libraries
+# =============================================================================
 import visa
 import time
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
 
+import sweep_functions as sweep
 import vna_functions as vna
 
 # =============================================================================
 # #Setup measurement
 # =============================================================================
 
-## Make contact with the VNA and initialise it
-rm = visa.ResourceManager()
-PNA = vna.connect('GPIB0::16::INSTR')
-vna.reset(PNA)
-vna.clear(PNA)
+PNA, rm = sweep.init()
+
+sweep.standard_sweep(PNA)
 
 # =============================================================================
 # Test code
 # =============================================================================
-#vna.meas_create(PNA, 'CH1_S11_1', 'S11')
 
-PNA.write(':CALCulate:PARameter:SELect "%s"' % ('CH1_S11_1'))
-time.sleep(1)
-temp_values = PNA.query_ascii_values(':CALCulate:DATA? %s' % ('FDATA'))
-
-
+meas_names = vna.get_meas_names(PNA)
 
 # =============================================================================
 # Reset VNA
