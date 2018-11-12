0dd29a18c972e73cd6a9683a918d25a68520b05f
Rijk <r.e.hogenbirk@gmail.com>
2018-11-12 18:25:10 +0900

    Made f_sweep to include new function and added T as a variable

--- VNA_f_sweep.py
+++ VNA_f_sweep.py
@@ -29,29 +29,33 @@ import vna_functions as vna
 # ## Input parameters
 # =============================================================================
 
-file_name       = 'calibration_11_7K'
+T               = 3.8
 
-#f_start         = 100      # MHz
-#f_stop          = 10000    # MHz
+file_name       = 'VNAdata_%sK' % (T)
+
+#
+#f_start         = 100       # MHz
+#f_stop          = 67000     # MHz
 f_start         = 4000      # MHz
-f_stop          = 6000    # MHz
+f_stop          = 6000      # MHz
 
-num_points      = 8001
+num_points      = 16001
 IF_bandwidth    = 200    # Hz
 
-amplitude       = -50     # dBm
+amplitude       = -50    # dBm
 
 #s_param         = ['S11']  # List of strings
-#s_param         = ['S21']
-s_param         = ['S21', 'S11', 'S22']
+s_param         = ['S21']
+#s_param         = ['S21', 'S11', 'S22']
 #s_param         = ['S21', 'S11']
 
 error_correction = 1
 
-#%%
-### Code
+#%% Code
 
 PNA, rm         = sweep.init()
+
+sweep.reset(PNA)
 
 #%%
 meas_names      = sweep.setup(PNA, f_start, f_stop, num_points, IF_bandwidth, amplitude, s_param)
@@ -59,16 +63,23 @@ meas_names      = sweep.setup(PNA, f_start, f_stop, num_points, IF_bandwidth, am
 #%% Autoscale
 vna.autoscale(PNA, 1)
 
+print('MESSAGE: Autoscale succesful')
+
 #%%
 data, elapsed   = sweep.get_data(PNA, error_correction=1)
 
+#%%
+full_file_name       = sweep.save_data(PNA, file_name, data, s_param)
+
+sweep.save_parameters(full_file_name, T, f_start, f_stop, num_points, IF_bandwidth, amplitude)
 
 #%%
-sweep.save_data(PNA, file_name, data, s_param)
+#sweep.plot_sdata(data, s_param, IF_bandwidth, only_S21=0)
 
-#%%
-sweep.plot_data(data, s_param, num_points, IF_bandwidth, only_S21=0)
-#sweep.plot_data(data, s_param, num_points, IF_bandwidth, only_S21=1)
+sweep.plot_sdata_mag(data, s_param, IF_bandwidth, amplitude, only_S21=0)
+
+plt.savefig(full_file_name + '.svg')
+print('MESSAGE: Plot saved')
 
 #%%
 sweep.close(PNA, rm)
