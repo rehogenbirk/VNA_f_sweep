f76bada8e9e84f0e659bfc87fd48d0dca284139d
Rijk <r.e.hogenbirk@gmail.com>
2018-11-15 18:42:51 +0900

    Made script that can load pickled figure

---
+++ load_saved_figure.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 15 15:54:07 2018
+
+@author: Rijk
+
+Loads pickled (saved) figures
+
+"""
+
+# Import libraries
+
+import pickle as pkl
+import matplotlib.pyplot as plt
+import os
+
+#%% Parameters
+
+figure_path     = r'C:\Users\Rijk\Documents\NAOJ Engineering\Vector Network Analyzer\Data_git\Data Third Light 181112\Ratios'
+
+figure_name     = 'ratio_bla1_and_bla2'
+
+#%% Get figure and open it
+
+figure_file     = os.path.join(figure_path, figure_name + '.pkl')
+
+with open(figure_file, 'rb') as fid:
+    fig = pkl.load(fid)
+
+plt.show()  
+
+axis    = fig.gca()
+
+#%% Change plot parameters
+
+xlabel  = 'Frequency (Hz)'
+ylabel  = 'Magnitude (dB)'
+title   = 'Ratio of transmission with closed 300K window and laser'
+legend  = ['Closed window', 'Laser']
+
+## Format the figures
+
+# Sets origin in upper-left corner
+#plt.axis([min(xs), max(xs), min(ys), max(ys)]) 
+
+# Turns on grid
+plt.grid(True)
+axis.xaxis.grid(which='minor')
+axis.yaxis.grid(which='minor')
+
+# Sets labels, legend and title
+plt.xlabel(xlabel)
+plt.ylabel(ylabel)
+plt.legend(legend)
+plt.title(title)
