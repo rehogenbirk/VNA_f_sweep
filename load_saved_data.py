14b1c074c661e9d9e62039a1b407ba12de2df912
Rijk <r.e.hogenbirk@gmail.com>
2018-11-20 11:32:33 +0900

    Updated script to load saved data with new data_analysis module

--- load_saved_data.py
+++ load_saved_data.py
@@ -12,11 +12,13 @@ Created on Thu Oct 11 15:33:57 2018
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import data_analysis as dat
 
 ## Parameters
 
-file_name = 'VNAdataS21_with_laser.csv'
-file_path = r'C:\Users\Rijk\Documents\NAOJ Engineering\Vector Network Analyzer\Data for Delft 181010'
+file_path = r'C:\Users\Rijk\Documents\NAOJ Engineering\Vector Network Analyzer\Data_git\Data Third Light 181112\Ratios\Laser 181115 data'
+file_name = 'ratio_column5_and_column7'
+
 #file = file_path + file_name
 
 file = os.path.join(file_path, file_name) 
@@ -30,48 +32,34 @@ f       = 'f'
 s_param = ['S21']
 IF_bandwidth = 200 # Hz
 
-## Code
+#%% Code
 
 #IF_bandwidth    = input('What was the IF bandwidth for this data? (Hz)\n')
 # Make these redundant by adding them to the saved data
 
-for s in s_param:
-    names = [f, s]
 
-units = [f_unit, s_unit]
+data    = dat.load_data(file)
 
-saved_data = np.loadtxt(file, delimiter=delimiter)
-
-fs = saved_data[0,:]
-data = saved_data[1:,:]
+fs      = dat.get_fs(file)
 
 f_start = np.min(fs)
 f_stop  = np.max(fs)
 num_points  = len(data[0])
 
 
-## Changing the data and storing it again
-#saving_data = np.ones((len(saved_data[:,0]), len(saved_data[0,:])+2))
-#saving_data[:,0] = names
-#saving_data[:,1] = units
-#saving_data[:,1:] = saved_data
-    
-# Transposing data
-saving_data = np.zeros((saved_data.T.shape[0]-1, saved_data.T.shape[1] + 1))
-saving_data[:, :saved_data.T.shape[1]] = saved_data.T[1:,:]
+#%% Changing the data and storing it again
+
 
 data_name = 'test' + '.csv'
-np.savetxt(data_name, saving_data, delimiter=',')
+np.savetxt(data_name, saved_data, delimiter=',')
 
 
-## Plotting
+#%% Plotting
 
-plt.figure(3)
-for i in range(len(data[:,0])):
-    plt.plot(fs, data[i])
+plt.plot(fs, data[0])
 
 # Format the figures
-plt.axis([min(fs), max(fs), -10, -5]) # Sets origin in upper-left corner
+#plt.axis([min(fs), max(fs), -10, -5]) # Sets origin in upper-left corner
 #plt.axis([min(fs), max(fs), -40, 0]) # Sets origin in upper-left corner
 plt.grid(True)
 
